6c3665436b32d8254ce9409119916e99ee54966f
Andrei.Pozolotin <Andrei.Pozolotin@gmail.com>
2013-04-08 14:30:00 -0500

    change provider implementation

--- one-api-provider/src/main/java/tester/one/api/provider/ServiceProvider.java
+++ one-api-provider/src/main/java/tester/one/api/provider/ServiceProvider.java
@@ -1,8 +1,12 @@
 package tester.one.api.provider;
 
+import tester.one.api.Listener;
 import tester.one.api.Service;
 
 public class ServiceProvider implements Service {
+
+	protected void bind(final Listener listerner) {
+	}
 
 	@Override
 	public void process_1() {
