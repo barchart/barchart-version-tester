17d27a2bae178b2bb0a8489e8fbb47bcfb17101b
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2013-06-03 22:36:09 -0500

    a

---
+++ one-api-provider/src/main/java/tester/one/impl/provider/HiddenEncoder.java
@@ -0,0 +1,14 @@
+package tester.one.impl.provider;
+
+import org.apache.commons.codec.Encoder;
+import org.apache.commons.codec.EncoderException;
+
+public class HiddenEncoder implements Encoder {
+
+	@Override
+	public Object encode(final Object source) throws EncoderException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
