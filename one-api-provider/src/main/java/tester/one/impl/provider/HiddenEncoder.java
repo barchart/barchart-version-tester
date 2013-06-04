853f0813c96e2345765ee12aec2d68ab4ebe1b2d
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2013-06-03 23:43:44 -0500

    a

--- one-api-provider/src/main/java/tester/one/impl/provider/HiddenEncoder.java
+++ one-api-provider/src/main/java/tester/one/impl/provider/HiddenEncoder.java
@@ -2,11 +2,22 @@ package tester.one.impl.provider;
 
 import org.apache.commons.codec.Encoder;
 import org.apache.commons.codec.EncoderException;
+import org.apache.commons.net.ftp.FTP;
+import org.joda.time.DateTime;
+
+import tester.one.api.consumer.ListenerProvider;
 
 public class HiddenEncoder implements Encoder {
 
 	@Override
 	public Object encode(final Object source) throws EncoderException {
+
+		new DateTime();
+
+		new ListenerProvider();
+
+		new FTP();
+
 		// TODO Auto-generated method stub
 		return null;
 	}
